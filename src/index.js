84207f1f3bd2542e95dc21d0e003a8151e75e244
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-18 22:42:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,51 +5,43 @@ import debounce from 'lodash.debounce';
 import ulCountry from '../src/templates/ul.hbs';
 import cardContainer from '../src/templates/card.hbs';
 
-import {fetchCountries} from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryCard = document.querySelector('.country-info');
 
-
-
-
-
-
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-
-
-
 function onInput(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let searchQuery = event.target.value;
-    searchQuery = searchQuery.trim();
+  let searchQuery = event.target.value;
+  searchQuery = searchQuery.trim();
 
-    fetchCountries(searchQuery)
-    .then(renderCountryCard)
-    .catch(onFetchError)
+  fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
 }
-
 
 function renderCountryCard(country) {
-    countryList.innerHTML = '';
-    countryCard.innerHTML = '';
+  countryList.innerHTML = '';
+  countryCard.innerHTML = '';
 
-    if(country.length > 10) {
-        Notiflix.Notify.success('Too many matches found. Please enter a more specific name.');
-        return;
-    } if(country.length >= 2) {
-        countryList.innerHTML = ulCountry(country);
-        return;
-    } 
-        countryCard.innerHTML = cardContainer(country[0]);
+  if (country.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  }
+  if (country.length >= 2) {
+    countryList.innerHTML = ulCountry(country);
+    return;
+  }
+  countryCard.innerHTML = cardContainer(country[0]);
 }
 
-function onFetchError (error) {
-    if(error.message === '404') {
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-    }
+function onFetchError(error) {
+  if (error.message === '404') {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
 }
